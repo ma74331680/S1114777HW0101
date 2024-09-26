aa8d2e0efd45a80acc8c8f47a87dcc9790a64ee3
max792489 <s1114777@o365st.pu.edu.tw>
2024-09-26 16:02:50 +0800

    加入專案檔案。

---
+++ S1114777HW0101/Animal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+class Animal
+{
+    protected string _name;
+    protected string _call;
+    protected int _count;
+    public static int _objCreateTimes;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public string Call
+    {
+        get { return _call; }
+        set { _call = value; }
+    }
+
+    public int Count
+    {
+        get { return _count; }
+        set { _count = value; }
+    }
+
+    public Animal()
+    {
+        _name = "animal";
+        _call = "null";
+        _count = 0;
+        _objCreateTimes++;
+    }
+
+    public void Say()
+    {
+        Console.WriteLine($"{_count} {_name} say {_call}");
+    }
+}
+
+class Cat : Animal
+{
+    public string Name
+    {
+        get { return _name; }
+    }
+    public string Call
+    {
+        get { return _call; }
+    }
+    public void EatFish()
+    {
+        Console.WriteLine("The cat just ate a fish");
+    }
+
+    public Cat()
+    {
+        _name = "cat";
+        _call = "meow~";
+        _count = 0;
+        _objCreateTimes++;
+    }
+}
+  
+class Tool
+{
+    public static void Sleep(string name)
+    {
+        Console.WriteLine($"The {name} is sleeping now");
+    }
+
+    public static void ShowNumOfObj()
+    {
+        Console.WriteLine($"We have {Animal._objCreateTimes} objects");
+    }
+}
---
+++ S1114777HW0101/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{   
+    static void Main()
+    {
+        Tool.ShowNumOfObj();
+        Animal animal1 = new Animal();
+        animal1.Say();
+        Tool.ShowNumOfObj();
+        Animal animal2 = new Animal();
+        animal2.Name = "dog";
+        animal2.Count = 2;
+        animal2.Call = "Woo~";
+        animal2.Say();
+        Tool.ShowNumOfObj();
+        Cat cat = new Cat();
+        cat.Count = 3;
+        cat.Say();
+        Tool.ShowNumOfObj();
+        cat.EatFish();
+        Tool.Sleep(cat.Name);
+    }
+}
